81cc40dd0b0ade812cbb2a3411e400ee8f2bc012
Arkaprava Choudhury <arkaprava.choudhury@outlook.com>
2021-10-26 12:17:21 -0400

    restructure project

---
+++ prelim/two_unitaries.py
@@ -0,0 +1,193 @@
+"""
+CSC299 ROP: Simple implementation
+"""
+
+import tequila as tq
+import copy
+from typing import Iterable
+from numpy import arcsin, sqrt, floor, pi, asarray
+from random import randint
+
+
+def prepare_2unitary(ancillary, sum_of_unitaries: list[tuple[float, tq.QCircuit]]) -> tq.QCircuit:
+    """
+    Prepare operator, when the Hamiltonian can be expressed as the linear combination of two
+    unitary operators.
+    Requires only one ancillary qubit.
+
+    Preconditions:
+        - ...
+    """
+    alpha_0, alpha_1 = sum_of_unitaries[0][0], sum_of_unitaries[1][0]
+
+    theta = 2 * arcsin(sqrt(alpha_1 / (alpha_0 + alpha_1)))
+
+    return tq.gates.Ry(target=ancillary, angle=theta)
+
+
+def select_2unitary(ancillary, unitary_0: tq.QCircuit, unitary_1: tq.QCircuit) -> tq.QCircuit:
+    """
+    Select operator, when the Hamiltonian can be expressed as the linear combination of two
+    unitary operators.
+    Requires only one ancillary qubit.
+
+    Returns ...
+    """
+    impl_1 = control_unitary(ancilla=ancillary, unitary=unitary_1)
+
+    x_gate = tq.gates.X(target=ancillary)
+    control = control_unitary(ancilla=ancillary, unitary=unitary_0)
+
+    impl_0 = x_gate + control + x_gate
+
+    return impl_1 + impl_0
+
+
+def control_unitary(ancilla, unitary: tq.QCircuit) -> tq.QCircuit:
+    """Return controlled version of unitary
+
+    SHOULD NOT mutate unitary
+
+    Preconditions:
+        - ancilla and unitary cannot have any common qubits
+    """
+    gates = unitary.gates
+    cgates = []
+    for gate in gates:
+        cgate = copy.deepcopy(gate)
+        if isinstance(ancilla, Iterable):
+            control_lst = list(cgate.control) + list(ancilla)
+        else:
+            control_lst = list(cgate.control) + [ancilla]
+        cgate._control = tuple(control_lst)
+        cgate.finalize()
+        cgates.append(cgate)
+
+    return tq.QCircuit(gates=cgates)
+
+
+def example_function() -> tq.QCircuit:
+    """Test example for LCU with two unitary matrices"""
+    identity = tq.gates.X(1) + tq.gates.X(1)
+    unitaries = [(0.5, identity), (0.5, tq.gates.Z(1))]
+    return algorithm_2unitary(unitaries=unitaries)
+
+
+def lcu_2unitary(nonunitary: tq.numpy.array, sum_of_unitaries: list[tuple[float, tq.QCircuit]]) \
+        -> tq.QCircuit:
+    """Linear combinations of unitaries for the case with two unitaries.
+
+    Preconditions:
+        - len(unitaries) == 2
+    """
+    if check_weighted_sum(nonunitary, sum_of_unitaries):
+        return algorithm_2unitary(sum_of_unitaries)
+    else:
+        raise ValueError
+
+
+def algorithm_2unitary(unitaries: list[tuple[float, tq.QCircuit]]) -> tq.QCircuit:
+    """The main part.
+
+    TODO: write proper docstring
+    """
+    prepare = prepare_2unitary(0, unitaries)
+    circ = prepare + select_2unitary(0, unitaries[0][1], unitaries[1][1]) + prepare.dagger()
+    return circ
+
+
+def check_weighted_sum(nonunitary: tq.numpy.array, unitaries: list[tuple[float, tq.QCircuit]]) \
+        -> bool:
+    """
+    Return whether the matrix given is indeed the weighted sum of the unitaries.
+
+    TODO
+    """
+    ...
+
+
+def amp_amp(unitaries: list[tuple[float, tq.QCircuit]], walk_op: tq.QCircuit, ancilla) \
+        -> tq.QCircuit:
+    """Amplitude amplification procedure obtained by repeating the amplitude amplification
+    step for a total of s times where s is the result of function _num_iter()
+    """
+    amplification_operator = amp_amp_op(walk_op, ancilla)
+    s = _num_iter(unitaries)
+
+    sum_of_steps = tq.QCircuit()
+    for _ in range(s):
+        sum_of_steps += amplification_operator
+
+    return walk_op + sum_of_steps
+
+
+def amp_amp_op(walk_op: tq.QCircuit, ancilla) -> tq.QCircuit:
+    """Return WRW.dagger()R,
+     where R is the reflect operator returned by the func reflect_operator"""
+    anc_qubits = ancilla if isinstance(ancilla, list) else [ancilla]
+    state_qubits = [qubit for qubit in walk_op.qubits if qubit not in anc_qubits]
+
+    reflect = reflect_operator(state_qubits=state_qubits, ancilla=ancilla)
+
+    return reflect + walk_op.dagger() + reflect + walk_op
+
+
+def reflect_operator(state_qubits, ancilla) -> tq.QCircuit:
+    """
+    Return the reflection operator R = (I - 2P) \\otimes I_N,
+    where:
+        - I is the identity operator over the ancilla,
+        - P is the projector onto the 0 state for the ancilla,
+        - I_N is the identity operator over the state register
+
+    """
+    if isinstance(state_qubits, list) and isinstance(ancilla, list):
+        qubits = list(set(state_qubits + ancilla))
+    elif isinstance(state_qubits, list) and not isinstance(ancilla, list):
+        qubits = list(set(state_qubits + [ancilla]))
+    elif not isinstance(state_qubits, list) and isinstance(ancilla, list):
+        qubits = list(set([state_qubits] + ancilla))
+    else:
+        qubits = list(set([state_qubits] + [ancilla]))
+
+    z_gate, cz_gate = tq.gates.Z(target=qubits), tq.gates.Z(control=ancilla, target=state_qubits)
+
+    return z_gate + cz_gate
+
+
+def _num_iter(unitaries: list[tuple[float, tq.QCircuit]]) -> int:
+    """Return the number of times to apply the amplitude amplificiation to maximize
+    success probability"""
+    s = sum(pair[0] for pair in unitaries)
+    alpha = arcsin(1 / s)
+    frac = (pi / 2) / alpha
+    return floor(0.5 * (frac - 1))
+
+
+# Testing amplitude amplification
+
+# TODO
+
+
+def test_amp_amp() -> None:
+    """Test whether amp amp works..."""
+    n = randint(1, 4)
+    coeff = [sqrt(0.5) for _ in range(2 ** n)]
+
+    uniform = tq.QubitWaveFunction.from_array(asarray(coeff))
+
+    expected = ...
+
+
+def test_algorithm(op: tq.QCircuit,
+                   unitaries: list[tuple[float, tq.QCircuit]], ancilla) -> bool:
+    """Test whether this works at all. If it doesn't, welp, I'm in trouble."""
+
+    ...
+
+
+# Main tests
+
+
+if __name__ == '__main__':
+    ...
---
+++ test/test.py
@@ -0,0 +1,226 @@
+"""
+CSC299 ROP
+Testing file
+"""
+
+# Import statements
+import tequila as tq
+from math import sqrt, isclose
+from numpy import arcsin, asarray
+from lcu_v1 import LCU, lcu_1ancilla, prepare_operator, reflect_operator
+from typing import Union
+from random import random
+
+
+# Implement testing functions
+
+# TODO
+
+############################################################################################
+# Test 1 qubit ancilla
+############################################################################################
+
+#####################
+# Test prepare
+#####################
+
+# For reference, prepare operator is replicated here
+def _prepare_1ancilla(ancilla: Union[list[Union[str, int]], str, int],
+                      unitaries: list[tuple[float, tq.QCircuit]]) -> tq.QCircuit:
+    """
+    Prepare operator, when the Hamiltonian can be expressed as the linear combination of two
+    unitary operators.
+    Requires only one ancillary qubit.
+
+    Preconditions:
+        - if isinstance(ancilla, list): len(ancilla) == 1
+        - all(coeff > 0 for coeff in [pair[0] for pair in unitaries])
+    """
+    alpha_0, alpha_1 = unitaries[0][0], unitaries[1][0]
+
+    theta = 2 * arcsin(sqrt(alpha_1 / (alpha_0 + alpha_1)))
+
+    return tq.gates.Ry(target=ancilla, angle=theta)
+
+
+def test_prepare_1anc() -> None:
+    """Test whether the prepare operator for 1 qubit ancilla works as intended
+
+    Preconditions:
+        - 0 not in state_prep.qubits
+    """
+    num0 = random()
+    num1 = 1 - num0
+    unitaries = [(num0, tq.gates.X(1)), (num1, tq.gates.Z(1))]
+    prepare = _prepare_1ancilla(0, unitaries)
+
+    wfn_target = tq.QubitWaveFunction.from_array(asarray([sqrt(num0), sqrt(num1)]))
+
+    projector = tq.paulis.Projector(wfn=wfn_target)
+
+    expval = tq.ExpectationValue(H=projector, U=prepare)
+
+    p = tq.simulate(expval)
+
+    # print(p)
+    assert isclose(p, 1, abs_tol=0.001)
+
+
+#####################
+# Test select
+#####################
+
+# For reference, select operator is replicated below
+def _select_1ancilla(ancillary: Union[str, int], unitary_0: tq.QCircuit, unitary_1: tq.QCircuit) \
+        -> tq.QCircuit:
+    """
+    Select operator, when the Hamiltonian can be expressed as the linear combination of two
+    unitary operators.
+    Requires only one ancillary qubit.
+
+    Returns the select operator.
+
+    Preconditions:
+        - ancillary not in unitary_0.qubits
+        - ancillary not in unitary_1.qubits
+    """
+    impl_1 = unitary_1.add_controls(ancillary, inpl=False)
+
+    x_gate = tq.gates.X(target=ancillary)
+    control = unitary_0.add_controls(ancillary, inpl=False)
+
+    impl_0 = x_gate + control + x_gate
+
+    return impl_1 + impl_0
+
+
+def test_select_1anc() -> None:
+    """Test whether the select operator for the 1 qubit ancilla works as expected."""
+    ...
+
+# Test LCU
+
+
+############################################################################################
+# Test 2 qubit ancilla
+############################################################################################
+
+# Test prepare
+
+
+############################################################################################
+# Test general case
+############################################################################################
+
+# Test prepare
+
+def test_prepare_general() -> None:
+    """Test whether the general case for the prepare operator works as expected."""
+    n = 0
+    while n == 0:
+        n = int(4 * random())
+
+    length = 2 ** n
+    ancilla = list(range(length))
+    coeffs = [0.5 + 0.49 * random() for _ in range(length)]
+
+    unitaries = [(coeff, tq.gates.X(16)) for coeff in coeffs]
+
+    norm = sqrt(sum(coeffs))
+    coefficients = [sqrt(coeff) / norm for coeff in coeffs]
+    wfn_target = tq.QubitWaveFunction.from_array(asarray(coefficients))
+
+    prepare = prepare_operator(ancilla=ancilla, unitaries=unitaries)
+
+    fid = abs(wfn_target.inner(tq.simulate(prepare))) ** 2
+
+    assert isclose(fid, 1, abs_tol=0.001)
+
+
+# Test select
+
+
+############################################################################################
+# Amplitude amplification
+############################################################################################
+
+def amp_amp_op(walk_op: tq.QCircuit, ancilla) -> tq.QCircuit:
+    """Return W R W.dagger() R,
+     where R is the reflect operator returned by the function reflect_operator"""
+    anc_qubits = ancilla if isinstance(ancilla, list) else [ancilla]
+    state_qubits = [qubit for qubit in walk_op.qubits if qubit not in anc_qubits]
+
+    reflect = reflect_operator(state_qubits=state_qubits, ancilla=ancilla)
+
+    return reflect + walk_op.dagger() + reflect + walk_op
+
+
+#####################
+# Test reflection
+#####################
+
+def test_reflection() -> None:
+    """Test whether the reflection operator works as intended."""
+    circ = tq.gates.H(0) + reflect_operator(1, 0)
+    wfn_target = tq.QubitWaveFunction.from_array(asarray([-1 / sqrt(2), 0, 1 / sqrt(2), 0]))
+
+    projector = tq.paulis.Projector(wfn=wfn_target)
+
+    expval = tq.ExpectationValue(H=projector, U=circ)
+
+    p = tq.simulate(expval)
+
+    assert isclose(p, 1, rel_tol=0.001)
+
+
+#####################
+# Test amp_amp_op
+#####################
+
+def test_amp_amp_stationary() -> None:
+    """Test whether the non-trivial stationary angles for amp_amp_op are as expected"""
+    # Create uniform superposition of ancilla: qubit 0
+    state_prep = tq.gates.H(0)
+
+    # Define target state
+    wfn_target = tq.QubitWaveFunction.from_array(asarray([- 1 / sqrt(2), 0, 1 / sqrt(2), 0]))
+
+    # Define amplitude amplification circuit
+    amp_amp = amp_amp_op(state_prep, 0)
+    circ = amp_amp + state_prep
+
+    fid = abs(wfn_target.inner(tq.simulate(circ))) ** 2
+
+    assert isclose(fid, 1.0, abs_tol=0.001)
+
+
+def test_amp_amp_change() -> None:
+    """Test whether the amp_amp_op works as expected for non-stationary angles"""
+    # TODO
+    # Create uniform superposition of ancilla: qubit 0
+    state_prep = tq.gates.H(0)
+
+    alpha_0 = random()
+
+    while alpha_0 not in {0, 1, 1 / sqrt(2)}:
+        alpha_0 = random()
+
+    alpha_1 = sqrt(1 - (alpha_0 ** 2))
+
+    # Define target state
+    wfn_target = tq.QubitWaveFunction.from_array(asarray([1 / sqrt(2), 0, - 1 / sqrt(2), 0]))
+
+    # Define amplitude amplification circuit
+    amp_amp = amp_amp_op(state_prep, 0)
+    projector = tq.paulis.Projector(wfn=wfn_target)
+
+    expval = tq.ExpectationValue(H=projector, U=amp_amp)
+
+    p = tq.simulate(expval)
+
+    print(p)
+
+
+# if __name__ == '__main__':
+#     import pytest
+#     pytest.main()
